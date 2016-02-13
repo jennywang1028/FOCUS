41aff0884e535783d69a973005988e35e4134084
AMANDA ONG <amandaong33@gmail.com>
2016-02-13 18:38:18 -0500

    Updated timer method

--- FOCUS/TimerViewController.swift
+++ FOCUS/TimerViewController.swift
@@ -12,30 +12,62 @@ class TimerViewController: UIViewController {
     
     var timer = NSTimer()
     var counter = 0
-    var min = 0
+    //var min = 0
     var sec = 60
     var timerOn = false
     
     @IBOutlet weak var timerBtn: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var chicken: UIButton!
-    
+    let timeInterval:NSTimeInterval = 1.0
+    let timerEnd:NSTimeInterval = 10.0
+    var timeCount:NSTimeInterval = 0.0
+
+    //Set timer
     @IBAction func timerPressed(sender: AnyObject) {
-        if min != 120{
-            min = min + 1
+        if timeCount != 120.0{
+            timeCount = timeCount + 1
         }
         else {
-            min = 0
+            timeCount = 0
         }
-        timerLabel.text = "\(min):00"
+        timerLabel.text = timeString(timeCount)
     }
 
+    //Tap hen to start timer
     @IBAction func chickenPressed(sender: AnyObject) {
-        counter = min
-        timerOn = true
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
+        timer = NSTimer.scheduledTimerWithTimeInterval(timeInterval, target:self, selector: Selector("updateTimer"), userInfo: nil, repeats: true)
     }
     
+    @IBAction func stopTimer(sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func resetTimer(sender: UIButton) {
+        timer.invalidate()
+        timeCount = 0.0
+        timerLabel.text = timeString(timeCount)
+    }
+    
+    func timeString(time:NSTimeInterval) -> String {
+        let minutes = Int(time)
+        let seconds = Double(time)*60 % 60
+        return String(format:"%02i:%02i",minutes,Int(seconds))
+    }
+    
+    func updateTimer(timer:NSTimer){
+        //timerLabel.text = timer.userInfo as? String
+            //timer that counts down
+            timeCount = timeCount - timeInterval
+            if timeCount <= 0 {  //test for target time reached.
+                timerLabel.text = "0:00"
+                timer.invalidate()
+            } else { //update the time on the clock if not reached
+                timerLabel.text = timeString(timeCount)
+            }
+    }
+    
+    /*
     func updateCounter() {
         if timerOn == true{
             min--
@@ -60,6 +92,7 @@ class TimerViewController: UIViewController {
             timerLabel.text = "\(min):00"
         }
     }
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,4 +104,5 @@ class TimerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 }
